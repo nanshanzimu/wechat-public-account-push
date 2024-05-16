9e8895c234a9949a73a7b070252cabd965bca567
nanshanzimu <2284181918@qq.com>
2024-05-16 23:53:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogvV_6FUwuAuFUrFq7F-VOIt3MLg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	V-SzxdEfq0ZdoHQZXt42TCrH3UdUikEGMNSxJ52FexU',
+      useTemplateId: 'V-SzxdEfq0ZdoHQZXt42TCrH3UdUikEGMNSxJ52FexU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	V-SzxdEfq0ZdoHQZXt42TCrH3UdUikEGMNSxJ52FexU',
+  CALLBACK_TEMPLATE_ID: 'V-SzxdEfq0ZdoHQZXt42TCrH3UdUikEGMNSxJ52FexU',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'gh_64eebdab6b5a',
+      id: 'ogvV_6F1g7TSr6o8yzblhTf_PVoQ',
     }
   ],
 
