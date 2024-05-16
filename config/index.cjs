3f887d4533d9192b05373e1ed16b336ca4637e50
nanshanzimu <2284181918@qq.com>
2024-05-16 23:48:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '亲爱的宁小姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ogvV_6FUwuAuFUrFq7F-VOIt3MLg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ogvV_6FUwuAuFUrFq7F-VOIt3MLg',
+      useTemplateId: '	V-SzxdEfq0ZdoHQZXt42TCrH3UdUikEGMNSxJ52FexU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
