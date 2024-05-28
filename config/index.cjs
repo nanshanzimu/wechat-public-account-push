9978e177b2b39b567eba313eb8531ebed7aeaf67
nanshanzimu <2284181918@qq.com>
2024-05-28 18:22:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '来自li的关心',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogvV_6FUwuAuFUrFq7F-VOIt3MLg',
+      id: '  ogvV_6FUwuAuFUrFq7F-VOIt3MLg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '124bpjvauk2Gb-8M7snz0zRT0_s_6xX5Uq19gOMvZDM',
+      useTemplateId: '  124bpjvauk2Gb-8M7snz0zRT0_s_6xX5Uq19gOMvZDM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '124bpjvauk2Gb-8M7snz0zRT0_s_6xX5Uq19gOMvZDM',
+  CALLBACK_TEMPLATE_ID: '  124bpjvauk2Gb-8M7snz0zRT0_s_6xX5Uq19gOMvZDM',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogvV_6F1g7TSr6o8yzblhTf_PVoQ',
+      id: '  ogvV_6F1g7TSr6o8yzblhTf_PVoQ',
     }
   ],
 
